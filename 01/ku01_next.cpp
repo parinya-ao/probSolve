ee18cfcbfd0edbb126da688509594f86f2bcb0cd
parinya-ao <flim.parinya.ao@gmail.com>
2024-11-27 18:56:50 +0700

    end week1

--- 01/ku01_next.cpp
+++ 01/ku01_next.cpp
@@ -1,117 +1,40 @@
-/**
- *   author: Parinya Aobaun
- *   created: 2024-11-26 15:28
- **/
 #pragma GCC optimize("Ofast,unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2,fma")
 #include <bits/stdc++.h>
 using namespace std;
 
-#define fastio                      \
-  ios_base::sync_with_stdio(false); \
-  cin.tie(NULL);                    \
-  cout.tie(NULL)
-#define ll long long
-#define ull unsigned long long
-#define ld long double
-#define pb push_back
-#define pf push_front
-#define mp make_pair
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define vi vector<int>
-#define vll vector<ll>
-#define umap unordered_map
-#define uset unordered_set
-#define fi first
-#define se second
-// Debugging macros
-#define debug(x) cerr << #x << " = " << (x) << endl;
-#define debugv(v)      \
-  cerr << #v << " = "; \
-  for (auto &i : v)    \
-    cerr << i << " ";  \
-  cerr << endl;
-
-// Constants
-const int MOD = 1e9 + 7;
-const int INF = 1e9;
-const ll LINF = 1e18;
-const ld EPS = 1e-9;
-
-// Utility Functions
-template <typename T>
-bool chmin(T &a, T b) { return b < a ? a = b, true : false; }
-template <typename T>
-bool chmax(T &a, T b) { return b > a ? a = b, true : false; }
-
-// GCD and LCM
-ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
-ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
-
-// Modular Arithmetic
-ll mod_add(ll a, ll b, ll m = MOD) { return (a + b) % m; }
-ll mod_sub(ll a, ll b, ll m = MOD) { return (a - b + m) % m; }
-ll mod_mul(ll a, ll b, ll m = MOD) { return (a * b) % m; }
-ll mod_pow(ll base, ll exp, ll m = MOD)
+int main()
 {
-  ll res = 1;
-  while (exp)
-  {
-    if (exp & 1)
-      res = (res * base) % m;
-    base = (base * base) % m;
-    exp >>= 1;
-  }
-  return res;
-}
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
-void findCycle(int node, vi &visited, vi &to, vll &cycle)
-{
-  int current = node;
-  int length = 0;
-
-  while (!visited[current])
-  {
-    visited[current] = 1;
-    current = to[current];
-    length++;
-  }
-  if (current == node)
-  {
-    cycle.emplace_back(length);
-  }
-}
-
-signed main()
-{
-  fastio;
   int n;
   cin >> n;
-
-  vi to(n + 1);
-  for (int i = 1; i <= n; ++i)
+  vector<int> mapping(n + 1);
+  for (int i = 1; i <= n; i++)
   {
-    cin >> to[i];
+    cin >> mapping[i];
   }
 
-  vi visited(n + 1, 0);
-  vll cycle;
-  for (int i = 1; i <= n; ++i)
+  vector<int> visited(n + 1, 0);
+  int rounds = 0;
+
+  for (int i = 1; i <= n; i++)
   {
-    if (!visited[i])
+    if (visited[i] == 0)
     {
-      findCycle(i, visited, to, cycle);
+      int current = i;
+      int cycle_length = 0;
+
+      while (visited[current] == 0)
+      {
+        visited[current] = 1;
+        current = mapping[current];
+        cycle_length++;
+      }
+      rounds = max(rounds, cycle_length);
     }
   }
-  int ans = 1;
-  for (auto len : cycle)
-  {
-    ans = lcm(ans, len);
-  }
-  cout << ans << "\n";
 
+  cout << rounds << "\n";
   return 0;
 }
