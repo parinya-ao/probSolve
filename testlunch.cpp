2901e71257e8edd794420c7be7a3c84d507733d8
parinya-ao <flim.parinya.ao@gmail.com>
2024-12-26 20:47:16 +0700

    update

--- testlunch.cpp
+++ testlunch.cpp
@@ -1,73 +1,8 @@
-/**
- *  Author: Parinya Aobaun
- *  Created: 2024-12-22 18:52
- **/
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define fastio                 \
-  ios::sync_with_stdio(false); \
-  cin.tie(NULL);
-#define int long long
-#define all(v) v.begin(), v.end()
+int main() {
+  cout << "Hello, World!" << endl;
 
-void _print() { cerr << "]\n"; }
-template <typename T, typename V>
-void print(const pair<T, V> &p);
-template <typename T>
-void print(const T &x);
-
-void print(int x) { cerr << x; }
-void print(long x) { cerr << x; }
-void print(unsigned x) { cerr << x; }
-void print(unsigned long x) { cerr << x; }
-void print(unsigned long long x) { cerr << x; }
-void print(float x) { cerr << x; }
-void print(double x) { cerr << x; }
-void print(long double x) { cerr << x; }
-void print(char x) { cerr << '\'' << x << '\''; }
-void print(const char *x) { cerr << '"' << x << '"'; }
-void print(const string &x) { cerr << '"' << x << '"'; }
-void print(bool x) { cerr << (x ? "true" : "false"); }
-
-template <typename T, typename V>
-void print(const pair<T, V> &p)
-{
-  cerr << "{";
-  print(p.first);
-  cerr << ", ";
-  print(p.second);
-  cerr << "}";
-}
-
-template <typename T>
-void print(const T &x)
-{
-  int f = 0;
-  cerr << "[";
-  for (auto &i : x)
-  {
-    if (f++)
-      cerr << ", ";
-    print(i);
   }
-  cerr << "]";
-}
-
-#define debug(x...)             \
-  cerr << "[" << #x << "] = ["; \
-  _print(x)
-
-signed main()
-{
-  fastio;
-  const int n = 10;
-  vector<vector<int>> adj1(n);
-  vector<int> adj2[n];
-
-  print(adj1);
-  cout << "\n hello world" << "\n";
-  print(adj2);
-
-  return 0;
-}
