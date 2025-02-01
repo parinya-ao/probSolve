8937eb4ee5bf159b911ca19566b0b3d9d0acec6c
parinya-ao <flim.parinya.ao@gmail.com>
2025-02-01 20:06:15 +0700

    Refactor climbing problem solution to use a Graph class and improve pathfinding logic

--- 08/fin60_climb.cpp
+++ 08/fin60_climb.cpp
@@ -1,86 +1,101 @@
-#include <iostream>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <vector>
+//#include <set>
+//#include <algorithm>
+//#include <climits>
 
 using namespace std;
 
-const int MAX_N = 100010;
 const int INF = 1e9;
 
-int n, m;
-vector<int> adj[MAX_N];
-vector<int> weights[MAX_N];
-int deg[MAX_N];
-
-void read_input()
+class Graph
 {
-  cin >> n >> m;
-  // Initialize degrees to 0
-  for (int u = 0; u < n; u++)
+private:
+  int H, W;
+
+  vector<string> path;
+  vector<pair<int, int>> move = {
+      {-1, 0}, // N
+      {1, 0},  // S
+      {0, -1}, // E
+      {0, 1},  // W
+  };
+  const vector<int> moveCost = {5, 0, 1, 1};
+
+  bool canMove(int r, int c)
   {
-    deg[u] = 0;
+    return (r >= 0 && r < H && c >= 0 && c < W && path[r][c] == '.' and path[r][c] != 'X');
   }
-  // Read edges
-  for (int i = 0; i < m; i++)
+
+public:
+  Graph(int _height, int _width) : H(_height), W(_width)
   {
-    int a, b, w;
-    cin >> a >> b >> w;
-    // Convert to 0-based indexing
-    a--;
-    b--;
-    // Add edges for undirected graph
-    adj[a].push_back(b);
-    adj[b].push_back(a);
-    weights[a].push_back(w);
-    weights[b].push_back(w);
-    deg[a]++;
-    deg[b]++;
+    path.resize(H);
   }
-}
 
-vector<int> dijkstra(int start)
-{
-  vector<int> dist(n, INF);
-  vector<bool> visited(n, false);
-  set<pair<int, int>> pq;
-
-  dist[start] = 0;
-  pq.insert({0, start});
-
-  while (!pq.empty())
+  void readPath()
   {
-    int u = pq.begin()->second;
-    pq.erase(pq.begin());
-
-    if (visited[u])
-      continue;
-    visited[u] = true;
-
-    for (int i = 0; i < deg[u]; i++)
+    for (int r = 0; r < H; r++)
     {
-      int v = adj[u][i];
-      int w = weights[u][i];
-
-      if (dist[v] > dist[u] + w)
-      {
-        dist[v] = dist[u] + w;
-        pq.insert({dist[v], v});
-      }
+      cin >> path[r];
     }
   }
-  return dist;
-}
+
+  int dijkstra()
+  {
+    vector<vector<int>> dist(H, vector<int>(W, INF));
+    set<pair<int, int>> pq; // {distance, vertex}
+
+    dist[H - 1][0] = 0;
+    pq.insert({0, W * (H - 1) + 0});
+
+    while (!pq.empty())
+    {
+      int cost = pq.begin()->first;
+      int pos = pq.begin()->second;
+      pq.erase(pq.begin());
+
+      int curRow = pos / W;
+      int curCol = pos % W;
+
+      if (curRow == 0 && curCol == W - 1)
+      {
+        return cost;
+      }
+      if (cost > dist[curRow][curCol])
+        continue;
+
+      for (int i = 0; i < move.size(); i++)
+      {
+        int newRow = curRow + move[i].first;
+        int newCol = curCol + move[i].second;
+        if (canMove(newRow, newCol))
+        {
+          int newcost = cost + moveCost[i];
+          if (newcost < dist[newRow][newCol])
+          {
+            dist[newRow][newCol] = newcost;
+            pq.insert({newcost, newRow * W + newCol});
+          }
+        }
+      }
+    }
+    return -1;
+  }
+};
+
+int H, W;
 
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
 
-  read_input();
-  vector<int> distances = dijkstra(0); // Start from vertex 0
-
-  // Print distance to last vertex
-  cout << distances[n - 1] << '\n';
+  cin >> H >> W;
+  Graph graph(H, W);
+  graph.readPath();
+  cout << graph.dijkstra() << "\n";
 
   return 0;
 }
